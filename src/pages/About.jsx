6adb481b6a62552acda8a9f1267b39f1ac63b2d9
ic3surf3r <novis.joao@gmail.com>
2023-05-24 18:38:13 +0200

    basic pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">GitHub Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        React app to search using the GitHub API
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+    </>
+  );
+}
+
+export default About;
